0d21cd1bbf88197ae6f32dd17472903ff0628440
Alyssa Horrocks & Janice Dunn <student@epicodus.com>
2015-08-04 11:46:22 -0700

    Individual point value specs passed

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -3,8 +3,18 @@ class String
     tile = self
     if tile == "a"
       tile = 1
-    else
+    elsif tile == "d"
       tile = 2
+    elsif tile == "b"
+      tile = 3
+    elsif tile == "f"
+      tile = 4
+    elsif tile == "k"
+      tile = 5
+    elsif tile == "j"
+      tile = 8
+    else
+      tile = 10
     end
     tile
   end
--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -9,4 +9,24 @@ describe('String#scrabble') do
   it('Returns a score for any two-point letter') do
     expect(("d").scrabble()).to(eq(2))
   end
+
+  it('Returns a score for any three-point letter') do
+    expect(("b").scrabble()).to(eq(3))
+  end
+
+  it('Returns a score for any four-point letter') do
+    expect(("f").scrabble()).to(eq(4))
+  end
+
+  it('Returns a score for any five-point letter') do
+    expect(("k").scrabble()).to(eq(5))
+  end
+
+  it('Returns a score for any eight-point letter') do
+    expect(("j").scrabble()).to(eq(8))
+  end
+
+  it('Returns a score for any ten-point letter') do
+    expect(("q").scrabble()).to(eq(10))
+  end
 end
